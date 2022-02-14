99ee33c5a7d5f2878b4e89fa2b9d93625648681a
Ray Sorenson <stngry78@gmail.com>
2022-02-14 12:52:55 -0600

    added js comments

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const body = document.body;
+    const body = document.body;   //save a few keystrokes. Probably not worth it
     
-    let button1 = document.createElement('button');
-    button1.textContent = 'Add Square';
-    button1.setAttribute('class', 'button');
-    body.appendChild(button1);
-    
-    
-    
-    const containerDiv = document.createElement('div');
-    containerDiv.setAttribute('class', 'flex-container');
-    body.append(containerDiv);
-    let divCount = 0;   
+    let button1 = document.createElement('button');  //create element
+    button1.textContent = 'Add Square';              //add text to element
+    button1.setAttribute('class', 'button');         //add class and classname to element
+    body.appendChild(button1);                       //adds element to html
     
     
-    button1.addEventListener('click', function() {
+    
+    const containerDiv = document.createElement('div');   //creating a flex container
+    containerDiv.setAttribute('class', 'flex-container'); //class for flex added
+    body.append(containerDiv);                            //container added to html
+    let divCount = 0;                                     //counter to increment div ID
+    
+    
+    button1.addEventListener('click', function() {       //listener that adds squares
         let blackSquare = document.createElement('div');
         blackSquare.setAttribute('class', 'square');
-        divCount++;
-        blackSquare.setAttribute('id', divCount);
+        divCount++;                                      //increments counter
+        blackSquare.setAttribute('id', divCount);        //applies counter to div ID
         
         
         blackSquare.addEventListener('mouseover', function() {
-            blackSquare.innerText = blackSquare.id;
+            blackSquare.innerText = blackSquare.id;       //adds text to new div
         })
         
         blackSquare.addEventListener('mouseout', function(){
-            blackSquare.innerText = '';
+            blackSquare.innerText = '';                   //removes text from new div
         })
 
         blackSquare.addEventListener('click', function() {
-            let nuColor = Math.floor(Math.random()*16777215).toString(16);
-            blackSquare.style.backgroundColor = '#' + nuColor;
+            let nuColor = Math.floor(Math.random()*16777215).toString(16);  //rando color generator
+            blackSquare.style.backgroundColor = '#' + nuColor;              //applies rando color to element
         })
 
         blackSquare.addEventListener('dblclick', function() {
-            if (blackSquare.id % 2 == 0) {
-                if (blackSquare.nextSibling) {
-                    blackSquare.nextSibling.remove();
+            if (blackSquare.id % 2 == 0) {                 //checks if square ID is even or odd
+                if (blackSquare.nextSibling) {             //check for a nextSibling (nexted IF statement)
+                    blackSquare.nextSibling.remove();      //remove next sibling
                 } else {
                     alert('No square to remove');
                 }
         } else {
-            if (blackSquare.previousSibling) {
-                blackSquare.previousSibling.remove();
+            if (blackSquare.previousSibling) {            //checks for previos sibling (nested if statement)
+                blackSquare.previousSibling.remove();     //remove previous sibling
             } else {
                 alert('No square to remove');
             }
         }
         })
             
-    containerDiv.append(blackSquare);
+    containerDiv.append(blackSquare);       //append div. 
     })
 
 })
